e40d43dd2e0ff75465a6b601ad388df396062987
Bernard Chisumo <bchisumo74@gmail.com>
2020-01-14 20:43:02 +0000

    Update the bugs on survey show page  to display the validation on UI

--- app/Http/Controllers/SurveryController.php
+++ app/Http/Controllers/SurveryController.php
@@ -12,11 +12,20 @@ class SurveryController extends Controller
     public  function  show(Questionnaire $questionnaire, $slug)
     {
          $questionnaire->load('questions.answers');
+
          return view('survey.show',compact('questionnaire'));
     }
 
     public  function  store()
     {
+        //dd(request()->all());
+
+        $data = request()->validate([
+
+            'responses.*.answer_id'     => 'required',
+            'responses.*.question_id'   => 'required',
+        ]);
+
         dd(request()->all());
     }
 }
--- resources/views/survey/show.blade.php
+++ resources/views/survey/show.blade.php
@@ -10,75 +10,36 @@
                 <form action="/surveys/{{ $questionnaire->id }}-{{ Str::slug($questionnaire->title) }}" method="post">
                     @csrf
 
-                     @foreach($questionnaire->questions as $key=>$question )
-
+                    @foreach($questionnaire->questions as $key => $question)
                         <div class="card mt-4">
-                            <div class="card-header"><strong>{{ $key +1  }}</strong>  {{ $question->question }}</div>
+                            <div class="card-header"><strong>{{ $key + 1 }}</strong> {{ $question->question }}</div>
 
                             <div class="card-body">
+
+                                @error('responses.' . $key . '.answer_id')
+                                 <small class="text-danger">{{ $message }}</small>
+                                @enderror
+
                                 <ul class="list-group">
-
                                     @foreach($question->answers as $answer)
-
                                         <label for="answer{{ $answer->id }}">
                                             <li class="list-group-item">
+                                                <input type="radio" name="responses[{{ $key }}][answer_id]" id="answer{{ $answer->id }}"
+                                                       {{ (old('responses.' . $key . '.answer_id') == $answer->id) ? 'checked' : '' }}
+                                                       class="mr-2" value="{{ $answer->id }}">
+                                                {{ $answer->answer }}
 
-                                                <input type="radio" name="responses[{{ $key }}][$answer->id]" id="answer{{ $answer->id }}"
-                                                       value="{{ $answer->id }}" class="mr-2">
-                                                    {{$answer->answer}}
-
-                                                <input type="hidden" name="responses[{{ $key }}][$question->id]" value="{{$question->id}}">
-
+                                                <input type="hidden" name="responses[{{ $key }}][question_id]" value="{{ $question->id }}">
                                             </li>
                                         </label>
-
                                     @endforeach
                                 </ul>
                             </div>
                         </div>
+                    @endforeach
 
-                     @endforeach
-
-                    <button type="submit" class="btn btn-dark">Complete Survey</button>
+                    <button class="btn btn-dark" type="submit">Complete Survey</button>
                 </form>
-
-
-{{--                <div class="card">--}}
-{{--                    <div class="card-header">Create Questionnaire</div>--}}
-
-{{--                    <div class="card-body">--}}
-{{--                        <form action="#" method="post">--}}
-
-{{--                            @csrf--}}
-
-{{--                            <div class="form-group">--}}
-{{--                                <label for="title">Title</label>--}}
-{{--                                <input type="text" name="title" class="form-control" id="exampleInputText1" aria-describedby="textHelp" placeholder="Enter Text">--}}
-{{--                                <small id="textHelp" class="form-text text-muted">Give your questionnaire a title that attracts attention .</small>--}}
-
-{{--                                @error('title')--}}
-{{--                                <small class="text-danger">{{ $message }}</small>--}}
-{{--                                @enderror--}}
-{{--                            </div>--}}
-
-{{--                            <div class="form-group">--}}
-{{--                                <label for="purpose">Purpose</label>--}}
-{{--                                <input type="text" name="purpose" class="form-control" id="exampleInputPurpose1" aria-describedby="purposeHelp" placeholder="Enter Purpose">--}}
-{{--                                <small id="textHelp" class="form-text text-muted">Giving a purpose will increase responses .</small>--}}
-
-{{--                                @error('purpose')--}}
-{{--                                <small class="text-danger">{{ $message }}</small>--}}
-{{--                                @enderror--}}
-{{--                            </div>--}}
-
-
-{{--                            <div class="form-group">--}}
-{{--                                <button type="submit" class="btn btn-primary">Create Questionnaire</button>--}}
-{{--                            </div>--}}
-
-{{--                        </form>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
             </div>
         </div>
     </div>
