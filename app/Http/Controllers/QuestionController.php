ab6551557d1de757ab57ef9a9ed32750213bd78c
Bernard Chisumo <bchisumo74@gmail.com>
2020-01-14 16:12:57 +0000

    Implemented the logic to store  information in the  questioncontroller

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -18,10 +18,18 @@ class QuestionController extends Controller
         //dd(request()->all());
 
         $data = request()->validate([
-            'question.question' => 'required',  // check on ui
+            'question.question' => 'required',  // check on ui as notatiojn
             'answers.*.answer' => 'required'  // check on ui on answers[][answer]
 
         ]);
+
+
+        $question  = $questionnaire->questions()->create($data['question']);
+        $question->answers()->createMany($data['answers']);
+
+        return redirect('/questionnaire/'. $questionnaire->id);
+
+
     }
 
 }
