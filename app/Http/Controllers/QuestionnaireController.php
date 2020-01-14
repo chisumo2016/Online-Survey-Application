15b3b51fdcdeb4f764ecce17053ad31797ef6f20
Bernard Chisumo <bchisumo74@gmail.com>
2020-01-14 13:13:28 +0000

    Implemented the store method in questionnaire by using the relationships

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class QuestionnaireController extends Controller
 {
+
+    public  function  __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public  function  create()
     {
         return view('questionnaire.create');
@@ -21,12 +27,11 @@ class QuestionnaireController extends Controller
 
         ]);
 
-        //1:
+        /*1:$data['user_id'] = auth()->user()->id;
+        $questionnaire = Questionnaire::create($data);*/
 
-        $data['user_id'] = auth()->user()->id;
-
-
-        $questionnaire = Questionnaire::create($data);
+       //2: Saving through relationship
+        $questionnaire = auth()->user()->questionnaires()->create($data);
 
         return redirect('/questionnaires/' . $questionnaire->id);
 
