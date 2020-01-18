d9fbb9db9be356da32e64c57d56349f7ef76da7c
Bernard Chisumo <bchisumo74@gmail.com>
2020-01-18 23:23:09 +0000

    Added some login in questionnaire show method

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -40,7 +40,8 @@ class QuestionnaireController extends Controller
     public  function  show(Questionnaire  $questionnaire )
     {
         //lazy loading
-        $questionnaire->load('questions.answers');
+        //$questionnaire->load('questions.answers');
+        $questionnaire->load('questions.answers.responses');
 
         return view('questionnaire.show',compact('questionnaire'));
     }
