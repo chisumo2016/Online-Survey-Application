56096992fce6c19ea389e2bdd20d4a2273c879de
Bernard Chisumo <bchisumo74@gmail.com>
2020-01-18 23:20:32 +0000

    Added some relationship inside inside the answer called responses

--- app/Answer.php
+++ app/Answer.php
@@ -13,4 +13,10 @@ class Answer extends Model
     {
         return $this->belongsTo(Question::class);
     }
+
+
+    public  function  responses()
+    {
+        return $this->hasMany(SurveyResponse::class);
+    }
 }
